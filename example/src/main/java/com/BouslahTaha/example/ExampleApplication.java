6899361211b54d3a56f154152e4c93dfd2bcb42a
Bouslah Taha <bouslahtaha33@gmail.com>
2023-12-19 02:57:51 +1300

    test REST API

--- example/src/main/java/com/BouslahTaha/example/ExampleApplication.java
+++ example/src/main/java/com/BouslahTaha/example/ExampleApplication.java
@@ -2,12 +2,20 @@ package com.BouslahTaha.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "hello";
 	}
 
 }
